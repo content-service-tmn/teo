1ecf78ffde1356ad82f36d731c4e9a4248f58b99
vasilstar <vasilstar97@gmail.com>
2018-08-09 15:23:35 +0500

    checkpoint

--- site/templates/common/_head.php
+++ site/templates/common/_head.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <title>Document</title>
-<link rel="stylesheet" type="text/css" href= <?= $config->urls->templates . "assets/css/reset.css"?>>
-<link rel="stylesheet" type="text/css" href= <?= $config->urls->templates . "assets/css/style.css"?>>
-<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,600,700,800" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href= <?= $config->urls->templates . "assets/css/template.css"?>>
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700,800,900" rel="stylesheet">
--- site/templates/layouts/home.php
+++ site/templates/layouts/home.php
@@ -1,233 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="../assets/uikit/css/uikit.min.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/reset.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,600,700,800" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../assets/uikit/css/components/slider.min.css">
+<section class="header">
+  <div class="header__logo logo">
+    <div class="logo__link"></div>
+  </div>
+  <div class="header__nav nav">
 
-    <script type="text/javascript" src="../assets/js/jquery.min.js"></script>
-    <script type="text/javascript" src="../assets/uikit/js/uikit.min.js"></script>
-    <script type="text/javascript" src="../assets/uikit/js/components/slider.min.js"></script>
-</head>
-<body>
-
-<!-- HEADER -->
-<header class="header">
-    <div class="container container_center">
-        <div class="header__phone">
-            <a href="tel:+7 (4012) 52 44 07" class="header__phone-number">8-401-252-44-07</a>
-        </div>
-
-        <div class="header__flex">
-
-            <div class="header__logo">
-                <a href="#" class="header__link"><img src="../assets/img/logo.png"></a>
-            </div>
-
-            <!-- NAVIGATION -->
-            <nav class="header__nav">
-                <ul class="nav">
-                    <li class="nav__element"><a href="" class="nav__link">О нас</a></li>
-                    <li class="nav__element"><a href="" class="nav__link">Клиентам</a></li>
-                    <li class="nav__element"><a href="" class="nav__link">Важная информация</a></li>
-                    <li class="nav__element"><a href="" class="nav__link">Пресс-центр</a></li>
-                    <li class="nav__element"><a href="" class="nav__link">Контакты</a></li>
-                </ul>
-            </nav>
-
-        </div>
-        <!-- ПРИДУМАЙ НАЗВАНИЕ КЛАССАМ -->
-        <div class="map">
-            <h1 class="map__heading">Тюменское экологическое объединение</h1>
-            <p class="map__text">После падения Хазарии в степях господствовали, сменяя друг друга, тюркоязычные народы: печенеги, огузы и половцы. Их племена не обладали политическим единством</p>
-        </div>
-    </div>
-</header>
-
-<!-- ПЛИТКА - ТОЖЕ НУЖНО ПРИДУМАТЬ -->
-<section class="structure">
-    <div class="structure__bg">
-        <div class="container container_center">
-            <div class="structure__wrap">
-                <div class="structure__column">
-                    <div class="item item_big">
-                        <div class="item__wrap">
-                            <h4 class="item_medium__heading_big">Региональныйоператор пообращению с ТКОв Тюменской области</h4>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="structure__column">
-                    <div class="item item_medium">
-                        <div class="item__wrap">
-                            <h4 class="item_top__heading_small">Здесь будет слоган</h4>
-                        </div>
-                    </div>
-
-                    <div class="items_two">
-                        <div class="item item_small">
-                            <div class="item__wrap">
-                                <span class="item__heading_small">Документы</span>
-                            </div>
-                        </div>
-                        <div class="item item_small">
-                            <div class="item__wrap">
-                                <span class="item__heading_small">Закупки</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container container_center">
-        <div class="structure__wrap structure__wrap_padding">
-            <div class="structure__column">
-                <div class="item item_medium item_info">
-                    <div class="item__wrap">
-                        <h4 class="item__heading_small">Здесь будет слоган</h4>
-                    </div>
-                </div>
-            </div>
-            <div class="structure__column">
-                <div class="item item_medium item_info">
-                    <div class="item__wrap">
-                        <h4 class="item__heading_small">Здесь будет слоган</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="structure__wrap_large structure__wrap_padding">
-            <div class="item item_large">
-                <h2 class="item_top__heading_large">Здесь будет слоган</h2>
-                <p class="item_large__text">Многие люди, работа которых связана с компьютерами жалуются на проблемы со здоровьем, такие как головная боль, заболевания кисти, боли в спине, плохое зрение. </p>
-                <button class="button button_large">Сообщить о свалке</button>
-            </div>
-        </div>
-    </div>
+  </div>
 </section>
-
-<!-- NEWS -->
-<section class="news">
-    <div class="container container_center">
-        <div class="news__heading">
-            <h2 class="news__title">Новости</h2>
-            <div class="news__line"></div>
-            <div class="news__link button">
-                <a href="#" class="news__all">Все новости</a>
-            </div>
+<div class="container container_center">
+  <div class="grid grid_main">
+    <div class="grid__cell">
+      <div class="grid__wrapper">
+        <div class="item">
+          <h3 class="item__heading">региональный оператор по обращению с тко в тюменской области</h3>
+          <a href="" class="item__link"></a>
         </div>
-        <ul class="news-list">
-            <li class="news-list__element">
-                <div class="news-list__date">20 июня</div>
-                <h6 class="news-list__title"><a href="#" class="news-list__link">Гравитационный парадокс как интеллигибельный знак.</a></h6>
-                <p class="news-list__paragraph">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
-            </li>
-            <li class="news-list__element">
-                <div class="news-list__date">20 июня</div>
-                <h6 class="news-list__title"><a href="#" class="news-list__link">Гравитационный парадокс как интеллигибельный знак.</a></h6>
-                <p class="news-list__paragraph">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
-            </li>
-            <li class="news-list__element">
-                <div class="news-list__date">20 июня</div>
-                <h6 class="news-list__title"><a href="#" class="news-list__link">Гравитационный парадокс как интеллигибельный знак.</a></h6>
-                <p class="news-list__paragraph">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
-            </li>
-        </ul>
+      </div>
     </div>
-</section>
-
-<!-- GALLERY -->
-
-<!-- GALLERY -->
-<section class="gallery">
-    <div class="container container_center">
-        <div class="gallery__heading">
-            <h2 class="gallery__title">При поддержке</h2>
-            <div class="gallery__line"></div>
+    <div class="grid__cell">
+      <div class="grid-wrapper">
+        <div class="grid">
+          <div class="grid__cell grid__cell_big">
+            <div class="grid__wrapper">
+              <div class="item">
+                <h3 class="item__heading">здесь будет слоган</h3>
+                <a href="" class="item__link"></a>
+              </div>
+            </div>
+          </div>
+          <div class="grid__cell">
+            <div class="grid__wrapper">
+              <div class="item">
+                <h3 class="item__heading item__heading_small">документы</h3>
+                <a href="" class="item__link"></a>
+              </div>
+            </div>
+          </div>
+          <div class="grid__cell">
+            <div class="grid__wrapper">
+              <div class="item">
+                <h3 class="item__heading item__heading_small">закупки</h3>
+                <a href="" class="item__link"></a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="gallery__wrap">
-            <div data-uk-slider>
-                <div class="uk-slider-container">
-                    <ul class="uk-slider uk-grid-width-medium-1-4">
-                        <li class="gallery__item"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                        <li class="gallery__item"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                        <li class="gallery__item"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                        <li class="gallery__item"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                        <li class="gallery__item"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                        <li class="gallery__item"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                        <li class="gallery__item gallery__item_large"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                        <li class="gallery__item"><div class="gallery__image" style="background-image: url('../assets/img/test.jpg');"></div></li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-</section>
-
-
-
-
-
-
-<!-- FOOTER -->
-<section class="footer">
-    <div class="container container_center">
-        <div class="footer__logo"><a href="#"><img src="../assets/img/logo-color.png"></a></div>
-        <div class="footer__wrap">
-            <div class="footer__column">
-                <ul class="footer__list">
-                    <li class="footer__element"><a href="" class="footer__link">Связаться с нами</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Реестр недобросовестных поставщиков</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Прес центр</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Продукция и услуги</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Мусороперерабатывающий завод</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Годовой отчет</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Вакансии</a></li>
-                </ul>
-            </div>
-            <div class="footer__column">
-                <ul class="footer__list">
-                    <li class="footer__element"><a href="" class="footer__link">Связаться с нами</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Реестр недобросовестных поставщиков</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Прес центр</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Продукция и услуги</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Мусороперерабатывающий завод</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Годовой отчет</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Вакансии</a></li>
-                </ul>
-            </div>
-            <div class="footer__column">
-                <ul class="footer__list">
-                    <li class="footer__element"><a href="" class="footer__link">Связаться с нами</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Реестр недобросовестных поставщиков</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Прес центр</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Продукция и услуги</a></li>
-                    <li class="footer__element"><a href="" class="footer__link">Мусороперерабатывающий завод</a></li>
-                </ul>
-            </div>
-            <div class="footer__column">
-                <div class="footer__contacts">
-                    <div class="footer__contact"><span>Email </span><a href="malito:ecoteo@gmail.com" class="footer__link footer__link_bold">ecoteo@gmail.com</a></div>
-                    <div class="footer__contact"><span>телефон </span><a href="" class="footer__link footer__link_bold">8961-555-555</a></div>
-                    <div class="footer__contact"><span>факс </span><a href="" class="footer__link footer__link_bold">7(999)34-53-45</a></div>
-                    <div class="footer__icons">
-                        <a href="" class="footer__icon footer__icon_instagram"></a>
-                        <a href="" class="footer__icon footer__icon_vkontakte"></a>
-                        <a href="" class="footer__icon footer__icon_viber"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-</section>
-
-
-
-
-</body>
-</html>
+    <div class="grid__cell">
+      <div class="grid__wrapper">
+        <div class="item">
+          <h3 class="item__heading item__heading_small">вакансии</h3>
+          <p class="item__text">Здесь вы можете ознакомиться с важной информацией о нашей организации.</p>
+          <a href="" class="item__link"></a>
+        </div>
+      </div>
+    </div>
+    <div class="grid__cell">
+      <div class="grid__wrapper">
+        <div class="item">
+          <h3 class="item__heading item__heading_small">интересные факты</h3>
+          <p class="item__text">Здесь вы можете ознакомиться с важной информацией о нашей организации.</p>
+          <a href="" class="item__link"></a>
+        </div>
+      </div>
+    </div>
+    <div class="grid__cell grid__cell_big">
+      <div class="grid__wrapper">
+        <div class="proposal">
+          <h3 class="proposal__heading">увидели свалку?</h3>
+          <p class="proposal__text">Многие люди, работа которых связана с компьютерами жалуются на проблемы со здоровьем, такие как головная боль, заболевания кисти, боли в спине, плохое зрение.</p>
+          <a href="" class="proposal__button button">сообщить о свалке</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
