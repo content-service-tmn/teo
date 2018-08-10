6dcdc026f28745e2579d783f3fcac46a9238bd17
vasilstar <vasilstar97@gmail.com>
2018-08-10 12:28:04 +0500

    footer done

--- site/templates/layouts/home.php
+++ site/templates/layouts/home.php
@@ -1,9 +1,11 @@
 <section class="header">
-  <div class="header__logo logo">
-    <div class="logo__link"></div>
-  </div>
-  <div class="header__nav nav">
+  <div class="container container_center">
+    <a href="" class="header__logo logo">
+      <img src="http://ecoteo.ru/wp-content/uploads/2015/02/logoteo-350x90.png" class="logo__img" alt="">
+    </a>
+    <div class="header__nav nav">
 
+    </div>
   </div>
 </section>
 <section class="home">
@@ -139,6 +141,63 @@
       </div>
       <div class="support__cell">
         <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="footer">
+  <div class="container container_center">
+    <a href="" class="footer__logo logo">
+      <img src="http://ecoteo.ru/wp-content/uploads/2015/02/logoteo-350x90.png" class="logo__img" alt="">
+    </a>
+    <div class="footer__grid">
+      <div class="footer__cell">
+        <ul class="footer__list">
+          <li class="footer__item"><a href="" class="footer__link">связаться с нами</a></li>
+          <li class="footer__item"><a href="" class="footer__link">реестр недобросовестных поставщиков</a></li>
+          <li class="footer__item"><a href="" class="footer__link">пресс центр</a></li>
+          <li class="footer__item"><a href="" class="footer__link">продукция и услуги</a></li>
+          <li class="footer__item"><a href="" class="footer__link">мусороперерабатывающий завод</a></li>
+          <li class="footer__item"><a href="" class="footer__link">годовой отчет</a></li>
+          <li class="footer__item"><a href="" class="footer__link">вакансии</a></li>
+        </ul>
+      </div>
+      <div class="footer__cell">
+        <ul class="footer__list">
+          <li class="footer__item"><a href="" class="footer__link">связаться с нами</a></li>
+          <li class="footer__item"><a href="" class="footer__link">реестр недобросовестных поставщиков</a></li>
+          <li class="footer__item"><a href="" class="footer__link">пресс центр</a></li>
+          <li class="footer__item"><a href="" class="footer__link">продукция и услуги</a></li>
+          <li class="footer__item"><a href="" class="footer__link">мусороперерабатывающий завод</a></li>
+          <li class="footer__item"><a href="" class="footer__link">годовой отчет</a></li>
+          <li class="footer__item"><a href="" class="footer__link">вакансии</a></li>
+        </ul>
+      </div>
+      <div class="footer__cell">
+        <ul class="footer__list">
+          <li class="footer__item"><a href="" class="footer__link">связаться с нами</a></li>
+          <li class="footer__item"><a href="" class="footer__link">реестр недобросовестных поставщиков</a></li>
+          <li class="footer__item"><a href="" class="footer__link">пресс центр</a></li>
+          <li class="footer__item"><a href="" class="footer__link">продукция и услуги</a></li>
+          <li class="footer__item"><a href="" class="footer__link">мусороперерабатывающий завод</a></li>
+          <li class="footer__item"><a href="" class="footer__link">годовой отчет</a></li>
+          <li class="footer__item"><a href="" class="footer__link">вакансии</a></li>
+        </ul>
+      </div>
+      <div class="footer__cell">
+        <div class="contacts">
+          <ul class="contacts__list">
+            <li class="contacts__item"><a href="" class="contacts__link">email ecoteo@gmail.com</a></li>
+            <li class="contacts__item"><a href="" class="contacts__link">телефон 8961-555-555</a></li>
+            <li class="contacts__item"><a href="" class="contacts__link">факс +7(999)34-53-45</a></li>
+          </ul>
+          <div class="contacts__socials">
+            <a href="" class="contacts__social contacts__social_vk"></a>
+            <a href="" class="contacts__social contacts__social_vk"></a>
+            <a href="" class="contacts__social contacts__social_vk"></a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
