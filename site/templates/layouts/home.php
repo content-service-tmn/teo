be05008a344ceeabe7e09a9ab608d74a7e598438
vasilstar <vasilstar97@gmail.com>
2018-08-10 11:17:45 +0500

    main page doin

--- site/templates/layouts/home.php
+++ site/templates/layouts/home.php
@@ -6,72 +6,140 @@
 
   </div>
 </section>
-<div class="container container_center">
-  <div class="grid grid_main">
-    <div class="grid__cell">
-      <div class="grid__wrapper">
-        <div class="item">
-          <h3 class="item__heading">региональный оператор по обращению с тко в тюменской области</h3>
-          <a href="" class="item__link"></a>
-        </div>
-      </div>
-    </div>
-    <div class="grid__cell">
-      <div class="grid-wrapper">
-        <div class="grid">
-          <div class="grid__cell grid__cell_big">
-            <div class="grid__wrapper">
-              <div class="item">
-                <h3 class="item__heading">здесь будет слоган</h3>
-                <a href="" class="item__link"></a>
-              </div>
-            </div>
-          </div>
-          <div class="grid__cell">
-            <div class="grid__wrapper">
-              <div class="item">
-                <h3 class="item__heading item__heading_small">документы</h3>
-                <a href="" class="item__link"></a>
-              </div>
-            </div>
-          </div>
-          <div class="grid__cell">
-            <div class="grid__wrapper">
-              <div class="item">
-                <h3 class="item__heading item__heading_small">закупки</h3>
-                <a href="" class="item__link"></a>
-              </div>
-            </div>
+<section class="home">
+  <div class="container container_center">
+    <div class="grid grid_main">
+      <div class="grid__cell">
+        <div class="grid__wrapper">
+          <div class="item" style="background-image:url(https://www.w3schools.com/w3css/img_lights.jpg);">
+            <h3 class="item__heading">региональный оператор по обращению с тко в тюменской области</h3>
+            <a href="" class="item__link"></a>
           </div>
         </div>
       </div>
-    </div>
-    <div class="grid__cell">
-      <div class="grid__wrapper">
-        <div class="item">
-          <h3 class="item__heading item__heading_small">вакансии</h3>
-          <p class="item__text">Здесь вы можете ознакомиться с важной информацией о нашей организации.</p>
-          <a href="" class="item__link"></a>
+      <div class="grid__cell">
+        <div class="grid-wrapper">
+          <div class="grid">
+            <div class="grid__cell grid__cell_big">
+              <div class="grid__wrapper">
+                <div class="item">
+                  <h3 class="item__heading">здесь будет слоган</h3>
+                  <a href="" class="item__link"></a>
+                </div>
+              </div>
+            </div>
+            <div class="grid__cell">
+              <div class="grid__wrapper">
+                <div class="item" style="background-image:url(https://www.w3schools.com/w3css/img_lights.jpg);">
+                  <h3 class="item__heading item__heading_small">документы</h3>
+                  <a href="" class="item__link"></a>
+                </div>
+              </div>
+            </div>
+            <div class="grid__cell">
+              <div class="grid__wrapper">
+                <div class="item" style="background-image:url(https://www.w3schools.com/w3css/img_lights.jpg);">
+                  <h3 class="item__heading item__heading_small">закупки</h3>
+                  <a href="" class="item__link"></a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="grid__cell">
-      <div class="grid__wrapper">
-        <div class="item">
-          <h3 class="item__heading item__heading_small">интересные факты</h3>
-          <p class="item__text">Здесь вы можете ознакомиться с важной информацией о нашей организации.</p>
-          <a href="" class="item__link"></a>
+      <div class="grid__cell">
+        <div class="grid__wrapper">
+          <div class="item" style="background-image:url(https://www.w3schools.com/w3css/img_lights.jpg);">
+            <h3 class="item__heading item__heading_small">вакансии</h3>
+            <p class="item__text">Здесь вы можете ознакомиться с важной информацией о нашей организации.</p>
+            <a href="" class="item__link"></a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="grid__cell grid__cell_big">
-      <div class="grid__wrapper">
-        <div class="proposal">
-          <h3 class="proposal__heading">увидели свалку?</h3>
-          <p class="proposal__text">Многие люди, работа которых связана с компьютерами жалуются на проблемы со здоровьем, такие как головная боль, заболевания кисти, боли в спине, плохое зрение.</p>
-          <a href="" class="proposal__button button">сообщить о свалке</a>
+      <div class="grid__cell">
+        <div class="grid__wrapper">
+          <div class="item" style="background-image:url(https://www.w3schools.com/w3css/img_lights.jpg);">
+            <h3 class="item__heading item__heading_small">интересные факты</h3>
+            <p class="item__text">Здесь вы можете ознакомиться с важной информацией о нашей организации.</p>
+            <a href="" class="item__link"></a>
+          </div>
+        </div>
+      </div>
+      <div class="grid__cell grid__cell_big">
+        <div class="grid__wrapper">
+          <div class="proposal">
+            <h3 class="proposal__heading">увидели свалку?</h3>
+            <p class="proposal__text">Многие люди, работа которых связана с компьютерами жалуются на проблемы со здоровьем, такие как головная боль, заболевания кисти, боли в спине, плохое зрение.</p>
+            <a href="" class="proposal__button button">сообщить о свалке</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+</section>
+<section class="news">
+  <div class="container container_center">
+    <div class="news__header">
+      <h1 class="news__heading">новости</h1>
+      <div class="news__line"></div>
+      <a href="" class="news__button">все новости</a>
+    </div>
+    <div class="news__grid">
+      <div class="news__cell">
+        <div class="news-block">
+          <p class="news-block__date">20 июня</p>
+          <h3 class="news-block__heading">гравитационный парадокс как интеллигибельный знак.</h3>
+          <p class="news-block__text">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
+        </div>
+      </div>
+      <div class="news__cell">
+        <div class="news-block">
+          <p class="news-block__date">20 июня</p>
+          <h3 class="news-block__heading">Надстройка нетривиальна. Дискретность амбивалентно транспонирует гравитационный парадокс.</h3>
+          <p class="news-block__text">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
+        </div>
+      </div>
+      <div class="news__cell">
+        <div class="news-block">
+          <p class="news-block__date">20 июня</p>
+          <h3 class="news-block__heading">ЕВРОПЕЙСКИЙ МИКРОРАЙОН СКОРО начнёт озеленение</h3>
+          <p class="news-block__text">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="support">
+  <div class="container container_center">
+    <div class="support__header">
+      <h1 class="support__heading">при поддержке</h1>
+      <div class="support__line"></div>
+    </div>
+    <div class="support__grid">
+      <div class="support__cell">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+      <div class="support__cell">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+      <div class="support__cell">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+      <div class="support__cell support__cell_big">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+      <div class="support__cell">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+      <div class="support__cell">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+      <div class="support__cell">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+      <div class="support__cell">
+        <div class="support__image" style="background-image:url(https://previews.123rf.com/images/alekss/alekss1412/alekss141200404/34431214-background-of-a-green-grass-texture-green-lawn.jpg)"></div>
+      </div>
+    </div>
+  </div>
+</section>
