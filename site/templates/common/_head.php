9781ab626427ccd5c7e9898224129093fe3dc1d5
vasilstar <vasilstar97@gmail.com>
2018-08-12 21:47:23 +0500

    rukovodstvo

--- site/templates/common/_head.php
+++ site/templates/common/_head.php
@@ -2,7 +2,7 @@
 <title>Document</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href= <?= $config->urls->templates . "assets/css/template.css"?>>
-<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700,800,900" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,600,700,800,900" rel="stylesheet">
 <script
         src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
--- site/templates/layouts/layout_faq.php
+++ site/templates/layouts/layout_faq.php
@@ -1,1 +1,28 @@
-hello faq!
+
+<section class="faq">
+  <div class="container container_center">
+    <h2 class="faq__heading">вопрос-ответ</h2>
+    <div class="faq__grid uk-accordion" data-uk-accordion>
+      <div class="faq__cell">
+        <h3 class="uk-accordion-title">...</h3>
+        <div class="uk-accordion-content">...</div>
+
+        <h3 class="uk-accordion-title">...</h3>
+        <div class="uk-accordion-content">...</div>
+
+        <h3 class="uk-accordion-title">...</h3>
+        <div class="uk-accordion-content">...</div>
+      </div>
+      <div class="faq__cell">
+        <h3 class="uk-accordion-title">...</h3>
+        <div class="uk-accordion-content">...</div>
+
+        <h3 class="uk-accordion-title">...</h3>
+        <div class="uk-accordion-content">...</div>
+
+        <h3 class="uk-accordion-title">...</h3>
+        <div class="uk-accordion-content">...</div>
+      </div>
+    </div>
+  </div>
+</section>
