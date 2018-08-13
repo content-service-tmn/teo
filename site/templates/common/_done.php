ae3895a613aa6fafcc5a8a3babf980d1b4681e10
vasilstar <vasilstar97@gmail.com>
2018-08-13 12:55:57 +0500

    gogog

--- site/templates/common/_done.php
+++ site/templates/common/_done.php
@@ -12,6 +12,23 @@
 
 <?= $templateRender; ?>
 
+<script type="text/javascript">
+  function initMap() {
+    var coordinates= {lat: 57.1419482, lng: 65.5986856},
+        options = {
+          zoom: 16,
+          disableDefaultUI: true,
+          center: coordinates,
+          draggable: !("ontouchend" in document)
+        };
+    var map = new google.maps.Map(document.getElementById('google-map'), options);
+    $.getJSON('/google-map.json',function(data){
+      map.setOptions({styles:data});
+    });
+    new google.maps.Marker({map:map,position:coordinates});
+  }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDz-fa3z3jDQhfL6rwyNt3DEJ3XHbyoUHk&callback=initMap" async></script>
 
 </body>
 
