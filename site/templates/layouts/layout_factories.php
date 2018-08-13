473a0537f0e3f3b7579e5ac50916c3d5a592c1d8
vasilstar <vasilstar97@gmail.com>
2018-08-13 09:06:26 +0500

    заводы

--- site/templates/layouts/layout_factories.php
+++ site/templates/layouts/layout_factories.php
@@ -1,1 +1,71 @@
-hello factories!
+<div class="menu menu_green">
+  <div class="container container_center">
+    <div class="menu__grid">
+      <div class="menu__cell">
+        <a href="" class="menu__logo logo">
+          <img src="http://ecoteo.ru/wp-content/uploads/2015/02/logoteo-350x90.png" class="logo__img" alt="">
+        </a>
+      </div>
+      <div class="menu__cell">
+        <div class="menu__nav nav">
+          <a href="" class="nav__phone">8-800-250-73-26</a>
+          <ul class="nav__items">
+            <li class="nav__item"><a href="" class="nav__link">о нас</a></li>
+            <li class="nav__item"><a href="" class="nav__link">клиентам</a></li>
+            <li class="nav__item"><a href="" class="nav__link">важная информация</a></li>
+            <li class="nav__item"><a href="" class="nav__link">пресс-центр</a></li>
+            <li class="nav__item"><a href="" class="nav__link">контакты</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<section class="breadcrumbs">
+  <div class="container container_center">
+    <div class="breadcrumbs__grid">
+      <div class="breadcrumbs__cell"><a href="" class="breadcrumbs__link">главная</a></div>
+      <div class="breadcrumbs__cell"><a href="" class="breadcrumbs__link">клиентам</a></div>
+      <div class="breadcrumbs__cell"><a href="" class="breadcrumbs__link">полигоны тко</a></div>
+    </div>
+    <h2 class="breadcrumbs__heading">мусоросортировочные заводы</h2>
+  </div>
+</section>
+
+<section class="factories">
+  <div class="container container_center">
+    <div class="factories__grid">
+      <div class="factories__cell">
+        <img src="https://vignette.wikia.nocookie.net/cnc/images/2/2a/GLA_War_Factory_concept_art.jpg/revision/latest?cb=20130703095719" alt="" class="factories__image">
+      </div>
+      <div class="factories__cell">
+        <div class="factories__panel">
+          <h3 class="factories__subheading">завод такой</h3>
+          <p class="factories__text">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
+          <a href="" class="factories__link">перейти на страницу завода</a>
+        </div>
+      </div>
+      <div class="factories__cell">
+        <img src="https://vignette.wikia.nocookie.net/cnc/images/2/2a/GLA_War_Factory_concept_art.jpg/revision/latest?cb=20130703095719" alt="" class="factories__image">
+      </div>
+      <div class="factories__cell">
+        <div class="factories__panel">
+          <h3 class="factories__subheading">завод такой</h3>
+          <p class="factories__text">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
+          <a href="" class="factories__link">перейти на страницу завода</a>
+        </div>
+      </div>
+      <div class="factories__cell">
+        <img src="https://vignette.wikia.nocookie.net/cnc/images/2/2a/GLA_War_Factory_concept_art.jpg/revision/latest?cb=20130703095719" alt="" class="factories__image">
+      </div>
+      <div class="factories__cell">
+        <div class="factories__panel">
+          <h3 class="factories__subheading">завод такой</h3>
+          <p class="factories__text">Гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир гедонизм осмысляет дедуктивный метод. Согласно мнению известных философов, дедуктивный метод естественно порождает и обеспечивает мир</p>
+          <a href="" class="factories__link">перейти на страницу завода</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
