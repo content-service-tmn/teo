d82c9d13cf982fb4d1b33ceddc3fc311aeac8492
nikon72ru <nikon72ru@gmail.com>
2018-08-14 15:02:38 +0500

    chiefs done

--- site/templates/layouts/layout_chiefs.php
+++ site/templates/layouts/layout_chiefs.php
@@ -1,5 +1,4 @@
 <?php namespace Processwire; ?>
-
 <section class="breadcrumbs">
   <div class="container container_center">
     <div class="breadcrumbs__grid">
@@ -14,78 +13,17 @@
 <section class="chiefs">
   <div class="container container_center">
     <div class="chiefs__grid">
+      <?php bd($page->chiefs); ?>
+      <?php foreach($page->chiefs as $chief): ?>
       <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
+        <p class="chiefs__name"><?=$chief->chief_fio?></p>
+        <p class="chiefs__post"><?=$chief->chief_position?></p>
         <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
+          <li><a href="mailto:<?=$chief->chief_email?>" class="chiefs__mail"><?=$chief->chief_email?></a></li>
+          <li><a href="tel:<?=phoneLink($chief->chief_phones)?>" class="chiefs__phone"><?=$chief->chief_phones?></a></li>
         </ul>
       </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директор</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
-      <div class="chiefs__cell">
-        <p class="chiefs__name">Анна Иванова</p>
-        <p class="chiefs__post">Директолог директа агрегатной промышленности</p>
-        <ul class="chiefs__list">
-          <li><a href="" class="chiefs__mail">directorecoteo72@mail.com</a></li>
-          <li><a href="" class="chiefs__phone">+7 (999) 000-00-00</a></li>
-        </ul>
-      </div>
+      <?php endforeach; ?>
     </div>
   </div>
 </section>
