cef3f0ff5c2e617224e9e493cab8eca0b64bac6f
nikon72ru <nikon72ru@gmail.com>
2018-08-14 12:59:03 +0500

    procurements done.

--- site/templates/layouts/layout_procurements.php
+++ site/templates/layouts/layout_procurements.php
@@ -2,7 +2,8 @@
   <div class="container container_center">
     <div class="breadcrumbs__grid">
         <?php foreach ($page->parents()->append($page) as $parent): ?>
-            <div class="breadcrumbs__cell"><a href="<?= $parent->url ?>" class="breadcrumbs__link"><?= $parent->title ?></a></div>
+          <div class="breadcrumbs__cell"><a href="<?= $parent->url ?>"
+                                            class="breadcrumbs__link"><?= $parent->title ?></a></div>
         <?php endforeach; ?>
     </div>
   </div>
@@ -14,7 +15,9 @@
       <div class="document__left">
         <ul class="document__list">
             <?php foreach ($page->parent->children as $pge):; ?>
-              <li><a href="<?=$pge->url?>" class="document__link <?php if($pge->id == $page->id) echo "active"?>"><?=$pge->title?></a></li>
+              <li><a href="<?= $pge->url ?>"
+                     class="document__link <?php if ($pge->id == $page->id) echo "active" ?>"><?= $pge->title ?></a>
+              </li>
             <?php endforeach; ?>
         </ul>
       </div>
@@ -26,198 +29,87 @@
           </ul>
 
           <ul id="my-id" class="uk-switcher">
-            <li>
-              <div class="procurements__item">
-                <p class="procurements__heading">Номер закупки 27/2018</p>
-                <p class="procurements__subheading">Наименование закупки</p>
-                <p class="procurements__text">
-                  Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Наш знаках злых не точках прямо текст возвращайся строчка даль что, от всех семь. Однажды власти родного свое всемогущая деревни раз?
-                </p>
-                <div class="procurements__grid">
-                  <div class="procurements__cell">
-                    <p>Начало подачи заявок</p>
-                    <p>Конец подачи заявок</p>
-                  </div>
-                  <div class="procurements__cell procurements__cell_mid">
-                    <p>26.04.2018</p>
-                    <p>29.04.2018 18:00</p>
-                  </div>
-                  <div class="procurements__cell">
-                    <a href="" class="procurements__button">просмотреть на сайте</a>
-                  </div>
-                </div>
-                <div class="uk-accordion" data-uk-accordion="{showfirst:false}">
-                  <p class="uk-accordion-title"></p>
-                  <div class="uk-accordion-content">
-                    <div class="procurements__files">
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
+            <li id="current_procurements">
+                <?php foreach ($page->procurements as $procurement): if (!$procurement->procurement_isArchived): ?>
+                  <div class="procurements__item">
+                    <p class="procurements__heading">Номер закупки <?= $procurement->procurement_number ?></p>
+                    <p class="procurements__subheading"><?= $procurement->procurement_title ?></p>
+                    <p class="procurements__text">
+                        <?= $procurement->procurement_description ?>
+                    </p>
+                    <div class="procurements__grid">
+                      <div class="procurements__cell">
+                        <p>Начало подачи заявок</p>
+                        <p>Конец подачи заявок</p>
                       </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
+                      <div class="procurements__cell procurements__cell_mid">
+                        <p><?= $procurement->procurement_start ?></p>
+                        <p><?= $procurement->procurement_end ?></p>
                       </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
+                      <div class="procurements__cell">
+                        <a href="<?= $procurement->procurement_url ?>" class="procurements__button">просмотреть на
+                          сайте</a>
                       </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
-                      </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
-                      </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
+                    </div>
+                    <div class="uk-accordion" data-uk-accordion="{showfirst:false}">
+                      <p class="uk-accordion-title"></p>
+                      <div class="uk-accordion-content">
+                        <div class="procurements__files">
+                            <?php foreach ($procurement->procurement_docs as $doc): ?>
+                              <div class="procurements__file">
+                                <div class="document__file">
+                                  <p class="document__name"><?= $doc->description ?></p>
+                                  <a href="<?= $doc->url ?>" class="document__file-link">Ознакомиться с документом
+                                    (<?= strtoupper($doc->description) ?>, размер <?= $doc->filesizeStr ?>)</a>
+                                </div>
+                              </div>
+                            <?php endforeach; ?>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div class="procurements__item">
-                  <p class="procurements__heading">Номер закупки 27/2018</p>
-                  <p class="procurements__subheading">Наименование закупки</p>
-                  <p class="procurements__text">
-                    Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Наш знаках злых не точках прямо текст возвращайся строчка даль что, от всех семь. Однажды власти родного свое всемогущая деревни раз?
-                  </p>
-                  <div class="procurements__grid">
-                    <div class="procurements__cell">
-                      <p>Начало подачи заявок</p>
-                      <p>Конец подачи заявок</p>
-                    </div>
-                    <div class="procurements__cell procurements__cell_mid">
-                      <p>26.04.2018</p>
-                      <p>29.04.2018 18:00</p>
-                    </div>
-                    <div class="procurements__cell">
-                      <a href="" class="procurements__button">просмотреть на сайте</a>
-                    </div>
-                  </div>
-                  <div class="uk-accordion" data-uk-accordion="{showfirst:false}">
-                    <p class="uk-accordion-title"></p>
-                    <div class="uk-accordion-content">
-                      <div class="procurements__files">
-                        <div class="procurements__file">
-                          <div class="document__file">
-                            <p class="document__name">Документ</p>
-                            <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                          </div>
-                        </div>
-                        <div class="procurements__file">
-                          <div class="document__file">
-                            <p class="document__name">Документ</p>
-                            <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                          </div>
-                        </div>
-                        <div class="procurements__file">
-                          <div class="document__file">
-                            <p class="document__name">Документ</p>
-                            <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                          </div>
-                        </div>
-                        <div class="procurements__file">
-                          <div class="document__file">
-                            <p class="document__name">Документ</p>
-                            <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                          </div>
-                        </div>
-                        <div class="procurements__file">
-                          <div class="document__file">
-                            <p class="document__name">Документ</p>
-                            <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                          </div>
-                        </div>
-                        <div class="procurements__file">
-                          <div class="document__file">
-                            <p class="document__name">Документ</p>
-                            <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <?php endif; endforeach; ?>
             </li>
-            <li>
-              <div class="procurements__item">
-                <p class="procurements__heading">Номер закупки 27/2018</p>
-                <p class="procurements__subheading">Наименование закупки</p>
-                <p class="procurements__text">
-                  Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Наш знаках злых не точках прямо текст возвращайся строчка даль что, от всех семь. Однажды власти родного свое всемогущая деревни раз?
-                </p>
-                <div class="procurements__grid">
-                  <div class="procurements__cell">
-                    <p>Начало подачи заявок</p>
-                    <p>Конец подачи заявок</p>
-                  </div>
-                  <div class="procurements__cell procurements__cell_mid">
-                    <p>26.04.2018</p>
-                    <p>29.04.2018 18:00</p>
-                  </div>
-                  <div class="procurements__cell">
-                    <a href="" class="procurements__button">просмотреть на сайте</a>
-                  </div>
-                </div>
-                <div class="uk-accordion" data-uk-accordion="{showfirst:false}">
-                  <p class="uk-accordion-title"></p>
-                  <div class="uk-accordion-content">
-                    <div class="procurements__files">
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
+            <li id="archived_procurements">
+                <?php foreach ($page->procurements as $procurement): if ($procurement->procurement_isArchived): ?>
+                  <div class="procurements__item">
+                    <p class="procurements__heading">Номер закупки <?= $procurement->procurement_number ?></p>
+                    <p class="procurements__subheading"><?= $procurement->procurement_title ?></p>
+                    <p class="procurements__text">
+                        <?= $procurement->procurement_description ?>
+                    </p>
+                    <div class="procurements__grid">
+                      <div class="procurements__cell">
+                        <p>Начало подачи заявок</p>
+                        <p>Конец подачи заявок</p>
                       </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
+                      <div class="procurements__cell procurements__cell_mid">
+                        <p><?= $procurement->procurement_start ?></p>
+                        <p><?= $procurement->procurement_end ?></p>
                       </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
+                      <div class="procurements__cell">
+                        <a href="<?= $procurement->procurement_url ?>" class="procurements__button">просмотреть на
+                          сайте</a>
                       </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
-                      </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
-                        </div>
-                      </div>
-                      <div class="procurements__file">
-                        <div class="document__file">
-                          <p class="document__name">Документ</p>
-                          <a href="" class="document__file-link">Ознакомиться с документом (PDF, размер 1.22 мб)</a>
+                    </div>
+                    <div class="uk-accordion" data-uk-accordion="{showfirst:false}">
+                      <p class="uk-accordion-title"></p>
+                      <div class="uk-accordion-content">
+                        <div class="procurements__files">
+                            <?php foreach ($procurement->procurement_docs as $doc): ?>
+                              <div class="procurements__file">
+                                <div class="document__file">
+                                  <p class="document__name"><?= $doc->description ?></p>
+                                  <a href="<?= $doc->url ?>" class="document__file-link">Ознакомиться с документом
+                                    (<?= strtoupper($doc->description) ?>, размер <?= $doc->filesizeStr ?>)</a>
+                                </div>
+                              </div>
+                            <?php endforeach; ?>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                <?php endif; endforeach; ?>
             </li>
           </ul>
         </div>
