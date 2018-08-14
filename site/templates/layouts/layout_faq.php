1729a122fe7a60c9b99d971cff7c9c92e0a4d5a0
nikon72ru <nikon72ru@gmail.com>
2018-08-14 13:21:26 +0500

    faq without callback form done

--- site/templates/layouts/layout_faq.php
+++ site/templates/layouts/layout_faq.php
@@ -2,7 +2,8 @@
   <div class="container container_center">
     <div class="breadcrumbs__grid">
         <?php foreach ($page->parents()->append($page) as $parent): ?>
-            <div class="breadcrumbs__cell"><a href="<?= $parent->url ?>" class="breadcrumbs__link"><?= $parent->title ?></a></div>
+          <div class="breadcrumbs__cell"><a href="<?= $parent->url ?>"
+                                            class="breadcrumbs__link"><?= $parent->title ?></a></div>
         <?php endforeach; ?>
     </div>
     <h2 class="breadcrumbs__heading">вопрос-ответ</h2>
@@ -13,23 +14,19 @@
   <div class="container container_center">
     <div class="faq__grid uk-accordion" data-uk-accordion="{showfirst:false}">
       <div class="faq__cell">
-        <h3 class="uk-accordion-title">Очень важный и сложный вопрос ТЭО</h3>
-        <div class="uk-accordion-content">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Но переулка грустный там назад она встретил скатился одна, возвращайся напоивший прямо города вопроса они, составитель однажды, предупреждал буквенных? Эта!</div>
-        <h3 class="uk-accordion-title">Очень важный и сложный вопрос ТЭО</h3>
-        <div class="uk-accordion-content">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Но переулка грустный там назад она встретил скатился одна, возвращайся напоивший прямо города вопроса они, составитель однажды, предупреждал буквенных? Эта!</div>
-        <h3 class="uk-accordion-title">Очень важный и сложный вопрос ТЭО</h3>
-        <div class="uk-accordion-content">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Но переулка грустный там назад она встретил скатился одна, возвращайся напоивший прямо города вопроса они, составитель однажды, предупреждал буквенных? Эта!</div>
+          <?php foreach ($page->faq_item as $i => $quest): if ($i % 2 == 1): ?>
+            <h3 class="uk-accordion-title"><?= $quest->repeater_faq_quest ?></h3>
+            <div class="uk-accordion-content"><?= $quest->repeater_faq_answer ?></div>
+          <?php endif; endforeach; ?>
       </div>
       <div class="faq__cell">
-        <h3 class="uk-accordion-title">Очень важный и сложный вопрос ТЭО</h3>
-        <div class="uk-accordion-content">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Но переулка грустный там назад она встретил скатился одна, возвращайся напоивший прямо города вопроса они, составитель однажды, предупреждал буквенных? Эта!</div>
-        <h3 class="uk-accordion-title">Очень важный и сложный вопрос ТЭО</h3>
-        <div class="uk-accordion-content">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Но переулка грустный там назад она встретил скатился одна, возвращайся напоивший прямо города вопроса они, составитель однажды, предупреждал буквенных? Эта!</div>
-        <h3 class="uk-accordion-title">Очень важный и сложный вопрос ТЭО</h3>
-        <div class="uk-accordion-content">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Но переулка грустный там назад она встретил скатился одна, возвращайся напоивший прямо города вопроса они, составитель однажды, предупреждал буквенных? Эта!</div>
+          <?php foreach ($page->faq_item as $i => $quest): if ($i % 2 == 0): ?>
+            <h3 class="uk-accordion-title"><?= $quest->repeater_faq_quest ?></h3>
+            <div class="uk-accordion-content"><?= $quest->repeater_faq_answer ?></div>
+          <?php endif; endforeach; ?>
       </div>
     </div>
     <h3 class="faq__subheading">Не нашли ответ?</h3>
-    <p class="faq__text">Вы можете <a href="" class="faq__link">задать вопрос,</a>  мы обязательно Вам ответим.</p>
+    <p class="faq__text">Вы можете <a href="" class="faq__link">задать вопрос,</a> мы обязательно Вам ответим.</p>
   </div>
 </section>
