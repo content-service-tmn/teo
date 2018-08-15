70b2c98e896d58023e2657c3361a417dafb06477
vasilstar <vasilstar97@gmail.com>
2018-08-15 09:48:01 +0500

    погнали

--- site/templates/layouts/layout_galleries.php
+++ site/templates/layouts/layout_galleries.php
@@ -9,8 +9,46 @@
     </div>
 </section>
 
+<section class="photos">
+    <div class="container container_center">
+      <h3 class="photos__heading">Блок 1</h3>
+        <div class="photos__grid">
+            <div class="photos__cell photos__cell_small">
+                <div class="photos__item">
+                    <a href="https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg" class="photos__link" style="background-image:url(https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg);" data-uk-lightbox></a>
+                </div>
+            </div>
+            <div class="photos__cell photos__cell_small">
+                <div class="photos__item">
+                    <a href="https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg" class="photos__link" style="background-image:url(https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg);" data-uk-lightbox></a>
+                </div>
+            </div>
+            <div class="photos__cell photos__cell_small">
+                <div class="photos__item">
+                    <a href="https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg" class="photos__link" style="background-image:url(https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg);" data-uk-lightbox></a>
+                </div>
+            </div>
+            <div class="photos__cell photos__cell_small">
+                <div class="photos__item">
+                    <a href="https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg" class="photos__link" style="background-image:url(https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg);" data-uk-lightbox></a>
+                </div>
+            </div>
+            <div class="photos__cell photos__cell_small">
+                <div class="photos__item">
+                    <a href="https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg" class="photos__link" style="background-image:url(https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg);" data-uk-lightbox></a>
+                </div>
+            </div>
+            <div class="photos__cell photos__cell_small">
+                <div class="photos__item">
+                    <a href="https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg" class="photos__link" style="background-image:url(https://sun9-8.userapi.com/c831109/v831109158/16331c/c_Yh6qcGZ08.jpg);" data-uk-lightbox></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <section class="photos photos_padding-bottom">
     <div class="container container_center">
+      <h3 class="photos__heading">Блок 1</h3>
         <div class="photos__grid">
             <div class="photos__cell photos__cell_small">
                 <div class="photos__item">
