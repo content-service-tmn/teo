2c9f0728db84d61cf35e68180ce3ef598c0b9d85
Sancha_96 <Sancha_96@mail.ru>
2018-12-06 23:05:22 +0500

    changed a header and add a arrow in select

--- site/templates/layouts/layout_callback-full.php
+++ site/templates/layouts/layout_callback-full.php
@@ -39,6 +39,13 @@
             <div id="feedback_item_thema" class="feedback_form_item feedback_form_left  ">
 				<div class="feedback_form_caption">Тема обращения <span class="notnull_icon">*</span></div>
               <div class="feedback_form_field">
+                  <div class="feedback_form_field-arrow">
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 292.362 292.362" style="enable-background:new 0 0 292.362 292.362;" xml:space="preserve">
+<g>
+    <path d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424   C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428   s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z" fill="#bac2c2"/>
+</g>
+</svg>
+                  </div>
                 <select class="ecotek_inputtext">
                   <option value="Заключение договора" selected>Заключение договора</option>
                   <option value="Предложение о сотрудничестве">Предложение о сотрудничестве</option>
@@ -180,7 +187,14 @@
             <div id="feedback_item_target" class="feedback_form_item feedback_form_left">
 <div class="feedback_form_caption">Тема обращения <span class="notnull_icon">*</span></div>
               <div class="feedback_form_field">
-                <select class="ecotek_inputtext">
+                  <div class="feedback_form_field-arrow">
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 292.362 292.362" style="enable-background:new 0 0 292.362 292.362;" xml:space="preserve">
+<g>
+    <path d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424   C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428   s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z" fill="#bac2c2"/>
+</g>
+</svg>
+                  </div>
+                  <select class="ecotek_inputtext">
                   <option value="Заключение договора" selected>Заключение договора</option>
                   <option value="Предложение о сотрудничестве">Предложение о сотрудничестве</option>
                   <option value="Купить вторичное сырье">Купить вторичное сырье</option>
