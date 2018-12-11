38bd0b7434a44821a36cc5d49107464389a7e9d7
Sancha_96 <Sancha_96@mail.ru>
2018-12-11 15:32:14 +0500

    fix arrow in select

--- site/templates/layouts/layout_callback-full.php
+++ site/templates/layouts/layout_callback-full.php
@@ -92,17 +92,16 @@
                             <span class="notnull_icon">*</span>
                           <?php endif; ?></div>
                       <div class="feedback_form_field">
-                        <div class="feedback_form_field-arrow">
-                          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-                               id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 292.362 292.362"
-                               style="enable-background:new 0 0 292.362 292.362;" xml:space="preserve">
-<g>
-  <path d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424   C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428   s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z"
-        fill="#bac2c2"/>
-</g>
-</svg>
-                        </div>
-                        <select class="ecotek_inputtext">
+                          <span class="custom_form_span">
+                              <span class="custom_form_arrow" style="width: 30px; height: 30px; display: block;
+                                      background: url(<?= $config->urls->templates . 'assets/img/arrow-select.png' ?>) no-repeat;
+                                      background-position: center right; position: absolute">
+                              </span>
+                              <span class="custom_form_span_thema">
+                                  <?= $group->inputfields_select_variables[0]->procurement_title ?>
+                              </span>
+                          </span>
+                        <select class="ecotek_inputtext custom_form" style="cursor: pointer">
                             <?php foreach ($group->inputfields_select_variables as  $i => $variable): ?>
                               <option value="<?=$variable->procurement_title?>" <?php if ($i==0){echo 'selected';}?>><?=$variable->procurement_title?></option>
                             <?php endforeach; ?>
@@ -234,17 +233,16 @@
                         <span class="notnull_icon">*</span>
                       <?php endif; ?></div>
                   <div class="feedback_form_field">
-                    <div class="feedback_form_field-arrow">
-                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-                           x="0px" y="0px" width="20px" height="20px" viewBox="0 0 292.362 292.362"
-                           style="enable-background:new 0 0 292.362 292.362;" xml:space="preserve">
-<g>
-  <path d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424   C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428   s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z"
-        fill="#bac2c2"/>
-</g>
-</svg>
-                    </div>
-                    <select class="ecotek_inputtext">
+                    <span class="custom_form_span">
+                        <span class="custom_form_arrow" style="width: 30px; height: 30px; display: block;
+                                background: url(<?= $config->urls->templates . 'assets/img/arrow-select.png' ?>) no-repeat;
+                                background-position: center right; position: absolute">
+                        </span>
+                        <span class="custom_form_span_thema">
+                            <?= $group->inputfields_select_variables[0]->procurement_title ?>
+                        </span>
+                    </span>
+                    <select class="ecotek_inputtext custom_form" style="cursor: pointer">
                       <?php foreach ($group->inputfields_select_variables as  $i => $variable): ?>
                         <option value="<?=$variable->procurement_title?>" <?php if ($i==0){echo 'selected';}?>><?=$variable->procurement_title?></option>
                       <?php endforeach; ?>
