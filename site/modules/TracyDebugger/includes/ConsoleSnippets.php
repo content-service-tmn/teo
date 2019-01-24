32fa95d42b2fa7b5733fb9c70bfef8561bdb7621
Vladimir <nikon72ru@gmail.com>
2019-01-24 21:09:31 +0300

    .

--- site/modules/TracyDebugger/includes/ConsoleSnippets.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$configData = $this->wire('modules')->getModuleConfigData("TracyDebugger");
-$configData['snippets'] = $_POST['snippets'];
-$this->modules->saveModuleConfigData($this->wire('modules')->get("TracyDebugger"), $configData);
-exit;
